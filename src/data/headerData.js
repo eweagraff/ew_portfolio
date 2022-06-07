a971da6a2c5aa5aede8492b1909ab3c8265b2f2c
Emily Weagraff <weagraff.emily@gmail.com>
2022-06-07 15:46:59 -0700

    added profile picture

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,9 +1,10 @@
 import resume from '../assets/pdf/Ew_resume.pdf'
+import image from '../assets/png/face.png'
 
 export const headerData = {
-    name: 'Jane Doe',
-    title: "Web Developer",
+    name: 'Emily',
+    title: "Front-End Developer",
     desciption:"There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
-    image: 'https://raw.githubusercontent.com/PhantomScript/asset-container/main/developer-portfolio/landingImg.png',
+    image: image,
     resumePdf: resume
 }
